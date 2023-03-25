a0b66de30b7155b5a436a317dd76e2989d68afc6
pooya98 <pooya98@naver.com>
2023-03-25 23:45:04 +0900

    Week01 (2)

---
+++ Swift/Swift01/Hello_CommandLineTool/Hello_CommandLineTool/greeting.swift
@@ -0,0 +1,13 @@
+//
+//  greeting.swift
+//  Hello_CommandLineTool
+//
+//  Created by 강승우 on 2023/03/25.
+//
+
+import Foundation
+
+var obj2 = MyClass()
+
+// print("hello world")
+// 에러 발생 - Custom swift file에서는 top-level에 실행코드 작성 불가
