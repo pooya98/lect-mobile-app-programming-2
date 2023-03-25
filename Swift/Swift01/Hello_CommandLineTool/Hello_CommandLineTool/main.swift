ac2762d88e0f8fe4af780a7a43452f56ae9c3512
pooya98 <pooya98@naver.com>
2023-03-25 23:47:44 +0900

    Week01 (4)

--- Swift/Swift01/Hello_CommandLineTool/Hello_CommandLineTool/main.swift
+++ Swift/Swift01/Hello_CommandLineTool/Hello_CommandLineTool/main.swift
@@ -8,4 +8,20 @@
 import Foundation
 
 print("Hello, World!")
+// main.swift에서는 top-level에 실행코드 작성 가능
+
+class MyClass {
+    var intProperty = 0
+    var floatProperty : Float = 0.0
+    
+    // 에러 - 초기화되지 않는 프로퍼티는 ': Initializer 필요'
+    // var strProperty : String
+}
+
+var obj = MyClass()
+obj.intProperty = 10
+obj.floatProperty = 3.1
+
+let val = obj.floatProperty
+// val
 
